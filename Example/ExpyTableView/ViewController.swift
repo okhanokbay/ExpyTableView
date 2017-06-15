b7e1e7743e61d1f13227916a3cfcf2fefad4f609
Okhan <Okhan@Okhan-MacBook-Air.local>
2017-06-16 00:15:28 +0300

    Adding sample project.

--- Example/ExpyTableView/ViewController.swift
+++ Example/ExpyTableView/ViewController.swift
@@ -7,18 +7,85 @@
 //
 
 import UIKit
+import ExpyTableView
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+	
+	fileprivate enum CellType: Int {
+		case header, first, second, third
+	}
+	
+	@IBOutlet weak var expandableTableView: ExpyTableView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		expandableTableView.dataSource = self
+		expandableTableView.delegate = self
+	}
+	
+	func getHeaderCell(from tableView: UITableView) -> UITableViewCell {
+		return tableView.dequeueReusableCell(withIdentifier: String(describing: HeaderTableViewCell.self)) as! HeaderTableViewCell
+	}
 }
 
+extension ViewController: ExpyTableViewDataSource {
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 14
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 4
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		switch CellType(rawValue: indexPath.row)! {
+			
+		case .header:
+			return getHeaderCell(from: tableView)
+			
+		case .first:
+			return tableView.dequeueReusableCell(withIdentifier: String(describing: FirstTableViewCell.self)) as! FirstTableViewCell
+			
+		case .second:
+			return tableView.dequeueReusableCell(withIdentifier: String(describing: SecondTableViewCell.self)) as! SecondTableViewCell
+			
+		case .third:
+			return tableView.dequeueReusableCell(withIdentifier: String(describing: ThirdTableViewCell.self)) as! ThirdTableViewCell
+		}
+	}
+	
+	func expandingCell(forSection section: Int, inTableView tableView: ExpyTableView) -> UITableViewCell {
+		return getHeaderCell(from: tableView)
+	}
+}
+
+extension ViewController: ExpyTableViewDelegate {
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		print("row is selected: section: \(indexPath.section) row: \(indexPath.row)")
+	}
+	
+	func expyTableViewWillChangeState(withType type: ExpyActionType, forSection section: Int, inTableView tableView: ExpyTableView, animated: Bool) {
+		switch type {
+		case .expand:
+			print("WILL COLLAPSE")
+			
+		case .collapse:
+			print("WILL EXPAND")
+		}
+	}
+	
+	func expyTableViewDidChangeState(withType type: ExpyActionType, forSection section: Int, inTableView tableView: ExpyTableView, animated: Bool) {
+		switch type {
+		case .expand:
+			print("DID COLLAPSE")
+			
+		case .collapse:
+			print("DID EXPAND")
+			}
+	}
+}
+
+class FirstTableViewCell: UITableViewCell {}
+class SecondTableViewCell: UITableViewCell {}
+class ThirdTableViewCell: UITableViewCell {}
+class HeaderTableViewCell: UITableViewCell {}
