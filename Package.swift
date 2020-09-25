1a5d27f670790d29c3b4f9e4500517e0130fa462
Stephan Boner <stephan.boner@bluewin.ch>
2020-09-25 18:02:03 +0200

    Fixed Package.swift

--- Package.swift
+++ Package.swift
@@ -1,32 +1,26 @@
 // swift-tools-version:5.0
 
-let version = "1.2.1"
-
-
 import PackageDescription
 
 let package = Package(
-  name: "ExpyTableView",
-  platforms: [
-    .iOS(.v10)
-  ],
-  products: [
-    // Products define the executables and libraries produced by a package, and make them visible to other packages.
-    .library(
-      name: "ExpyTableView",
-      targets: ["ExpyTableView"]
-    )
-  ],
-  dependencies: [
-    // Dependencies declare other packages that this package depends on.
-  ],
-  targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-    .target(
-      name: "ExpyTableView",
-      dependencies: []
-    )
-  ],
-  swiftLanguageVersions: [.v5]
+    name: "ExpyTableView",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "ExpyTableView",
+            targets: ["ExpyTableView"]
+        )
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "ExpyTableView",
+            dependencies: [],
+            path: "ExpyTableView/Classes"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
 )
